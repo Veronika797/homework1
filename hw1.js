0867700760582d78e390597c8e7f80cf86d33fe8
Veronika797 <milchenkovd@gmail.com>
2024-11-03 13:11:47 +0300

    задание 7

--- hw1.js
+++ hw1.js
@@ -29,8 +29,17 @@
 // let result = power;
 // alert (result);
 
-let a = 9;
-let b = 2;
-let remainder = a / b;
-let result = remainder;
-alert (result);
+// let a = 9;
+// let b = 2;
+// let remainder = a / b;
+// let result = remainder;
+// alert (result);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
