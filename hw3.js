4420d01694edbddc2a77ad6609f300b0830a4576
Veronika797 <milchenkovd@gmail.com>
2024-11-06 13:27:04 +0300

    2.2 js

--- hw3.js
+++ hw3.js
@@ -12,7 +12,14 @@
 //     console.log('Пароль введен неправильно');
 // }
 
-let c = 2;
+// let c = 2;
+// if (c > 0 && c < 10) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+let c = 10;
 if (c > 0 && c < 10) {
     console.log('Верно');
 } else {
