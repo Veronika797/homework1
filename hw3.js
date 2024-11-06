4f65e786262a1915bd438ef3e7109215ab492f39
Veronika797 <milchenkovd@gmail.com>
2024-11-06 13:25:11 +0300

    2.1 js

--- hw3.js
+++ hw3.js
@@ -4,10 +4,17 @@
 // let password = (prompt('Введите пароль'));
 // alert(password);
 
-let password = 'пароль';
-let userPassword = prompt('Введите пароль');
-if (userPassword === password) {
-    console.log('Пароль введен верно');
+// let password = 'пароль';
+// let userPassword = prompt('Введите пароль');
+// if (userPassword === password) {
+//     console.log('Пароль введен верно');
+// } else {
+//     console.log('Пароль введен неправильно');
+// }
+
+let c = 2;
+if (c > 0 && c < 10) {
+    console.log('Верно');
 } else {
-    console.log('Пароль введен неправильно');
+    console.log('Неверно');
 }
