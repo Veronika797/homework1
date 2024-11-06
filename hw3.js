e07da0fc070e2e4fccc8c674485894a4a1c1a10d
Veronika797 <milchenkovd@gmail.com>
2024-11-06 13:27:39 +0300

    2.3 js

--- hw3.js
+++ hw3.js
@@ -19,7 +19,14 @@
 //     console.log('Неверно');
 // }
 
-let c = 10;
+// let c = 10;
+// if (c > 0 && c < 10) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+let c = -3;
 if (c > 0 && c < 10) {
     console.log('Верно');
 } else {
