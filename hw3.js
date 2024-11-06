e0ef0925091025d67fb13d095e147959ab9d25ce
Veronika797 <milchenkovd@gmail.com>
2024-11-06 13:21:37 +0300

    1.4 js

--- hw3.js
+++ hw3.js
@@ -8,4 +8,6 @@ let password = 'пароль';
 let userPassword = prompt('Введите пароль');
 if (userPassword === password) {
     console.log('Пароль введен верно');
-} ;
+} else {
+    console.log('Пароль введен неправильно');
+}
