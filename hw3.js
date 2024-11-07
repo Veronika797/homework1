fd0f0d4bac5437a5b17a9227a5e31af8c35443f6
Veronika797 <milchenkovd@gmail.com>
2024-11-07 17:53:35 +0300

    Resolve merge conflict by incorporating both suggestions

--- hw3.js
+++ hw3.js
@@ -45,32 +45,32 @@
 // let b = '3';
 // alert(Number(a) + Number(b));
 
-let monthNumber = 9;
-if (monthNumber < 1 || monthNumber > 12) {
-    ('Введите номер месяца от 1 до 12');
-} else {
-    switch (monthNumber) {
-        case 1:
-        case 2:
-        case 12:
-            alert('Зима');        
-            break;
-        case 3:
-        case 4:
-        case 5:
-            alert('Весна');
-            break;
-        case 6:
-        case 7:
-        case 8:
-            alert('Лето');
-            break;
-        case 9:alert
-        case 10:alert
-        case 11:alert
-            alert('Осень');
-            break;
-        default: 
-        console.log('Ошибка');
-    }
-}
+// let monthNumber = 9;
+// if (monthNumber < 1 || monthNumber > 12) {
+//     ('Введите номер месяца от 1 до 12');
+// } else {
+//     switch (monthNumber) {
+//         case 1:
+//         case 2:
+//         case 12:
+//             alert('Зима');        
+//             break;
+//         case 3:
+//         case 4:
+//         case 5:
+//             alert('Весна');
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:
+//             alert('Лето');
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:
+//             alert('Осень');
+//             break;
+//         default: 
+//         console.log('Ошибка');
+//     }
+// }
