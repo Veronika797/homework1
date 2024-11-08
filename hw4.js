60104347534d8ab83d8e5ba1e3678be0367d2243
Veronika797 <milchenkovd@gmail.com>
2024-11-08 16:09:59 +0300

    js 4

--- hw4.js
+++ hw4.js
@@ -12,11 +12,21 @@
 // }
 // }
 
-let n = 7;
- while (n <= 22) {
-     console.log(n);
-    n++;
-     if (n === 23) {
-    break;
+// let n = 7;
+//  while (n <= 22) {
+//      console.log(n);
+//     n++;
+//      if (n === 23) {
+//     break;
+// }
+//  }
+
+const obj = {
+    "Коля": '200',
+    "Вася": '300',
+    "Петя": '400'
 }
- }
+
+for (let key in obj) {
+    console.log(`${key}: ${obj[key]}`);
+}
