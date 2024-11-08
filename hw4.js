19c3385f6cd4ad375fd4c3292b12dd4644b60fbe
Veronika797 <milchenkovd@gmail.com>
2024-11-08 15:59:35 +0300

    js 3

--- hw4.js
+++ hw4.js
@@ -2,12 +2,21 @@
 //     console.log('Привет');
 // }
 
-let n = 1;
+// let n = 1;
 
-while (n <= 6) {
-    console.log(n);
+// while (n <= 6) {
+//     console.log(n);
+//     n++;
+//     if (n === 6) {
+//     break;
+// }
+// }
+
+let n = 7;
+ while (n <= 22) {
+     console.log(n);
     n++;
-    if (n === 6) {
+     if (n === 23) {
     break;
 }
-}
+ }
