dee7cec4d41d904eef7d17f76b54c2c2b9e74b7a
Veronika797 <milchenkovd@gmail.com>
2024-11-15 18:54:49 +0300

    задача 1

--- hw5.js
+++ hw5.js
@@ -1,19 +1,27 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-function guessNumber() {
-    let userGuess = prompt('Угадай число от 1 до 100');
-    userGuess = Number(userGuess);
-    if (userGuess === randomNumber) {
-        alert('Верно! Ты отгадал число');
-    } else if (userGuess < randomNumber) {
-        alert('Загаданное число больше. Попытай удачу еще раз');
-        guessNumber();
-    }
-    else if (userGuess > randomNumber) {
-        alert('Загаданное число меньше. Попытай удачу еще раз');
-        guessNumber();
-    }
-    else {
-        alert('Попытай удачу еще раз');
-    }
+// домашнее задание
+// const randomNumber = Math.floor(Math.random() * 100) + 1;
+// function guessNumber() {
+//     let userGuess = prompt('Угадай число от 1 до 100');
+//     userGuess = Number(userGuess);
+//     if (userGuess === randomNumber) {
+//         alert('Верно! Ты отгадал число');
+//     } else if (userGuess < randomNumber) {
+//         alert('Загаданное число больше. Попытай удачу еще раз');
+//         guessNumber();
+//     }
+//     else if (userGuess > randomNumber) {
+//         alert('Загаданное число меньше. Попытай удачу еще раз');
+//         guessNumber();
+//     }
+//     else {
+//         alert('Попытай удачу еще раз');
+//     }
+// }
+// guessNumber();
+
+// задача 1
+function min(a, b) {
+    return a < b ? a : b;
 }
-guessNumber();
+console.log(min(8, 4));
+console.log(min(6, 6));
