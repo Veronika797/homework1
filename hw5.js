6a033e2c96cacab0ed5834c156e5ad49a1819406
Veronika797 <milchenkovd@gmail.com>
2024-11-15 18:39:10 +0300

    создала первую игру

---
+++ hw5.js
@@ -0,0 +1,19 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+function guessNumber() {
+    let userGuess = prompt('Угадай число от 1 до 100');
+    userGuess = Number(userGuess);
+    if (userGuess === randomNumber) {
+        alert('Верно! Ты отгадал число');
+    } else if (userGuess < randomNumber) {
+        alert('Загаданное число больше. Попытай удачу еще раз');
+        guessNumber();
+    }
+    else if (userGuess > randomNumber) {
+        alert('Загаданное число меньше. Попытай удачу еще раз');
+        guessNumber();
+    }
+    else {
+        alert('Попытай удачу еще раз');
+    }
+}
+guessNumber();
