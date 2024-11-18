175ade931245f93cb35dc9d0e4765ff76c80d59b
Veronika797 <milchenkovd@gmail.com>
2024-11-18 16:09:15 +0300

    внесла поправки в игру

--- hw5.js
+++ hw5.js
@@ -1,19 +1,20 @@
 // домашнее задание
-const randomNumber = Math.floor(Math.random() * 100) + 1;
 function guessNumber() {
-    let userGuess = prompt('Угадай число от 1 до 100');
-    userGuess = Number(userGuess);
-    if (userGuess === randomNumber) {
-        alert('Верно! Ты отгадал число');
-    } else if (userGuess < randomNumber) {
-        alert('Загаданное число больше. Попытай удачу еще раз');
-        guessNumber();
-    }
-    else if (userGuess > randomNumber) {
-        alert('Загаданное число меньше. Попытай удачу еще раз');
-        guessNumber();
-    }
-    else {
-        alert('Попытай удачу еще раз');
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    let userGuess = 0;
+    while (userGuess !== randomNumber) {
+        userGuess =  prompt('Угадай число от 1 до 100');
+        userGuess = Number(userGuess);
+        if (userGuess === randomNumber) {
+            alert('Верно! Ты отгадал число');
+        } else if (userGuess < randomNumber) {
+            alert('Загаданное число больше. Попытай удачу еще раз');
+        }
+        else if (userGuess > randomNumber) {
+            alert('Загаданное число меньше. Попытай удачу еще раз');
+        }
+        else {
+            alert('Попытай удачу еще раз');
+        }
     }
 }
