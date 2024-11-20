dad568a3f1ad68a2e557984cb3ba96cd9c87b06b
Veronika797 <milchenkovd@gmail.com>
2024-11-20 15:29:47 +0300

    задача 1

--- hw6.js
+++ hw6.js
@@ -1,39 +1,127 @@
+// function generateTasks(min, max) {
+//     const operators = ['+', '-', '*', '/'];
+//     const num1 = Math.floor(Math.random() * (max - min + 1)) + min;
+//     const num2 = Math.floor(Math.random() * (max - min + 1)) + min;
+//     let operator = operators[Math.floor(Math.random() * operators.length)];
+//     if (operator === '/' && num2 === 0) {
+//         num2 = 1;
+//     }
+//     const randomTasks = `${num1} ${operator} ${num2}`;
+//     alert(randomTasks);
+//     let answer = prompt(Math.floor(parseFloat(`Посчитай и напиши ответ: ${randomTasks}`)));
+//     answer = parseFloat(answer);
+//     let correctAnswer;
+//     function roundUpNum() {
+//         let roundNum;
+//        if (correctAnswer % 1 >= 0.5) {
+//         roundNum = Math.ceil();
+//        } else {
+//         roundNum = Math.floor();
+//        }
+//        return roundNum;
+//     }
+//     switch (operator) {
+//         case '+':
+//             correctAnswer = num1 + num2;
+//             break;
+//         case '-':
+//             correctAnswer = num1 - num2;
+//             break;
+//         case '*':
+//             correctAnswer = num1 * num2;
+//             break;
+//         case '/':
+//            correctAnswer = num1 / num2;
+//             break;
+//         default:
+//             alert('Не правильный оператор');
+//             return;
+//     } 
+//     if(answer === correctAnswer) {
+//         alert('Ответ верный');
+//     }
+//     else {
+//         alert('Ошибка');
+//     }
+//     generateTasks(10, 1);
+// }
+
+
+// function generateTasks(min, max) {
+//     const operators = ['+', '-', '*', '/'];
+//     const num1 = Math.floor(Math.random() * (max - min + 1)) + min;
+//     const num2 = Math.floor(Math.random() * (max - min + 1)) + min;
+//     let operator = operators[Math.floor(Math.random() * operators.length)];
+//     if (operator === '/' && num2 === 0) {
+//         num2 = 1;
+//     }
+//     const randomTasks = `${num1} ${operator} ${num2}`;
+//     alert(randomTasks);
+//     let answer = prompt(Math.floor(parseFloat(`Посчитай и напиши ответ: ${randomTasks}`)));
+//     switch (operator) {
+//         case '+':
+//             answer = num1 + num2;
+//             break;
+//         case '-':
+//             answer = num1 - num2;
+//             break;
+//         case '*':
+//             answer = num1 * num2;
+//             break;
+//         case '/':
+//            answer = num1 / num2;
+//             break;
+//         default:
+//             alert('Не правильный оператор');
+//             return;
+//     } 
+//     if(randomTasks === answer) {
+//         alert('Ответ верный');
+//     }
+//     else {
+//         alert('Ошибка');
+//     }
+//     generateTasks(1, 100);
+// }
+
+
+
+
 function generateTasks(min, max) {
     const operators = ['+', '-', '*', '/'];
     const num1 = Math.floor(Math.random() * (max - min + 1)) + min;
     const num2 = Math.floor(Math.random() * (max - min + 1)) + min;
-  let value = NaN;
-  value = isNaN(value) ? 0 : value;
     let operator = operators[Math.floor(Math.random() * operators.length)];
     if (operator === '/' && num2 === 0) {
         num2 = 1;
     }
     const randomTasks = `${num1} ${operator} ${num2}`;
     alert(randomTasks);
-    let answer = Math.floor(parseFloat(`Посчитай и напиши ответ: ${randomTasks}`));
-    
+    let answer = prompt(`Посчитай и напиши ответ: ${randomTasks}`);
+    answer = parseFloat(answer);
     switch (operator) {
         case '+':
-            answer = num1 + num2;
+            correctAnswer = num1 + num2;
             break;
         case '-':
-            answer = num1 - num2;
+            correctAnswer = num1 - num2;
             break;
         case '*':
-            answer = num1 * num2;
+            correctAnswer = num1 * num2;
             break;
         case '/':
-            answer = num1 / num2;
+           correctAnswer = num1 / num2;
             break;
         default:
             alert('Не правильный оператор');
             return;
-    }
-    if(parseFloat(prompt(`Твой ответ для задачи: ${randomTasks}?`)) === answer) {
+    } 
+    if(correctAnswer === answer) {
         alert('Ответ верный');
     }
     else {
         alert('Ошибка');
     }
-    generateTasks(1, 100);
+    generateTasks(1, 5);
 }
+// generateTasks(1, 100);
---
+++ hw6tasks.js
@@ -0,0 +1,8 @@
+// задача 1
+const array = [1, 5, 4, 10, 0, 3];
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    if (array[i] === 10) {
+        break;
+    }
+}
