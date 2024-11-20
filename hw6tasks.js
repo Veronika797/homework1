b1cc2dd2a37fd2c4efb136ae563aa5c4b77047d4
Veronika797 <milchenkovd@gmail.com>
2024-11-20 15:34:45 +0300

    задача 2

--- hw6tasks.js
+++ hw6tasks.js
@@ -1,8 +1,13 @@
 // задача 1
+// const array = [1, 5, 4, 10, 0, 3];
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+//     if (array[i] === 10) {
+//         break;
+//     }
+// }
+
+// задача 2
 const array = [1, 5, 4, 10, 0, 3];
-for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-    if (array[i] === 10) {
-        break;
-    }
-}
+console.log(array.length);
+console.log(array[4]);
