b0bfba0f31af68bf1e9816b237dc14ab791973c1
Veronika797 <milchenkovd@gmail.com>
2024-11-28 19:34:58 +0300

    reversText

--- hw7.js
+++ hw7.js
@@ -1,5 +1,2 @@
-function reversText() {
-const text = document.getElementById("userInput").value;
-const reversed = text.split('').reverse().join('');
-document.getElementById('result').innerText = `Перевернутый текст: ${reversed}`;
-}
+// переверни текст
+function reversText() {}
