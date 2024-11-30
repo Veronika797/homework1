f06d1bd9fc84991edd1452912d38b1b1b44aae37
Veronika797 <milchenkovd@gmail.com>
2024-11-30 18:30:43 +0300

    игра викторина

--- hw7.js
+++ hw7.js
@@ -28,3 +28,39 @@ function gameReversText() {
    
 alert(`Игра окончена. Правильный ответ: ${origWord}`);
 }
+
+
+// викторина
+   const quiz = [
+      {
+          question: "Какой цвет небо?",
+          options: ["1. Красный", "2. Синий", "3. Зеленый"],
+          correctAnswer: 2 
+      },
+      {
+          question: "Сколько дней в неделе?",
+          options: ["1. Шесть", "2. Семь", "3. Восемь"],
+          correctAnswer: 2
+      },
+      {
+          question: "Сколько у человека пальцев на одной руке?",
+          options: ["1. Четыре", "2. Пять", "3. Шесть"],
+          correctAnswer: 2
+      }
+  ];
+  function gameQuiz() {
+   let score = 0;
+   for (let i = 0; i < quiz.length; i++) {
+      const currentQuestion = quiz[i];
+      let questionText = currentQuestion.question + "\n";
+      for (let j = 0; j < currentQuestion.options.length; j++) {
+         questionText += currentQuestion.options[j] + "\n";
+      }
+   const userAnswer = prompt(questionText);
+   const userAnswerInt = parseInt(userAnswer);
+   if (!isNaN(userAnswerInt) && userAnswerInt === currentQuestion.correctAnswer) {
+      score++;
+   }
+   }
+   alert("Количество правильных ответов: " + score);
+}
