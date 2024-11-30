c560f8984c2bc70ed364c556c99c17b8aab8e753
Veronika797 <milchenkovd@gmail.com>
2024-11-30 17:03:39 +0300

    игра переверни текст

--- hw7.js
+++ hw7.js
@@ -1,2 +1,30 @@
 // переверни текст
-function reversText() {}
+
+function getRandomWords() {
+   const words = ["земля", "солнце", "вода", "песок", "трава", "камень"];
+   return words[Math.floor(Math.random() * words.length)];
+}
+
+function reverseString(str) {
+   return str.split('').reverse().join('');
+}
+
+function gameReversText() {
+   const origWord = getRandomWords();
+   const reversedWord = reverseString(origWord);
+   let attempts = 3;
+   while (attempts > 0) {
+      const guess = prompt(`Угадай слово ${reversedWord}. Осталось попыток: ${attempts}`);
+      if (guess === origWord) {
+         alert("Правильно");
+         return;
+      } else {
+         attempts--;
+         if (attempts > 0) {
+           alert(`Не правильно. Попробуй снова`); 
+         }
+      }
+   }
+   
+alert(`Игра окончена. Правильный ответ: ${origWord}`);
+}
