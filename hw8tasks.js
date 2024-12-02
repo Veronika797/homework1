6a9791795fa688bca086b97b633b79091ea578e5
Veronika797 <milchenkovd@gmail.com>
2024-12-02 22:00:35 +0300

    task 2

--- hw8tasks.js
+++ hw8tasks.js
@@ -6,3 +6,32 @@ const people = [
     { name: 'Оксана', age: 47 }
  ];
  console.log(people.sort((a, b) => a.age - b.age));
+
+//  2
+function isPositive(item) {
+    return item >= 0;
+    }
+    function isMale(item) {
+    return item.gender === 'male';
+    }
+    function filter(arr, ruleFunction) {
+    const result = [];
+
+    arr.map(element => {
+        if (ruleFunction(element)) {
+            result.push(element);
+        }
+    })
+    return result;
+    }
+    
+    console.log(filter([3, -4, 1, 9], isPositive));
+    
+    const peoples = [
+       {name: 'Глеб', gender: 'male'},
+       {name: 'Анна', gender: 'female'},
+       {name: 'Олег', gender: 'male'},
+       {name: 'Оксана', gender: 'female'}
+    ];
+    
+    console.log(filter(peoples, isMale));
